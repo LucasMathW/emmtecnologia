3727b8f82cf54ea7000a978f61dac5dd91a2617b
De Alencar <lucasag0408@outlook.com>
2026-02-17 00:11:11 -0400

    only indentationgit add src/services/SettingServices/UpdateOneSettingService.ts

--- backend/src/services/SettingServices/UpdateOneSettingService.ts
+++ backend/src/services/SettingServices/UpdateOneSettingService.ts
@@ -2,31 +2,31 @@ import AppError from "../../errors/AppError";
 import Setting from "../../models/Setting";
 
 interface Request {
-    key: string;
-    value: string;
+  key: string;
+  value: string;
 }
 
 const UpdateOneSettingService = async ({
-    key,
-    value
+  key,
+  value
 }: Request): Promise<Setting | undefined> => {
-    const [setting] = await Setting.findOrCreate({
-        where: {
-            key
-        },
-        defaults: {
-            key,
-            value
-        }
-    });
-
-    if (!setting) {
-        throw new AppError("ERR_NO_SETTING_FOUND", 404);
+  const [setting] = await Setting.findOrCreate({
+    where: {
+      key
+    },
+    defaults: {
+      key,
+      value
     }
+  });
 
-    await setting.update({ value });
+  if (!setting) {
+    throw new AppError("ERR_NO_SETTING_FOUND", 404);
+  }
 
-    return setting;
+  await setting.update({ value });
+
+  return setting;
 };
 
 export default UpdateOneSettingService;
