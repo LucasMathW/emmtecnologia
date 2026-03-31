2d8eb8215f275f2d84488a8a27ceef92c919d56b
De Alencar <lucasag0408@outlook.com>
2026-03-31 00:58:56 -0400

    add resolveCompany middleware in public routes: login and singup

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -2,11 +2,11 @@ import express from "express";
 import isAuth from "../middleware/isAuth";
 import * as UserController from "../controllers/UserController";
 import * as SessionController from "../controllers/SessionController";
-
+import resolveCompany from "../middleware/resolveCompany";
 const authRoutes = express.Router();
 
-authRoutes.post("/signup", UserController.store);
-authRoutes.post("/login", SessionController.store);
+authRoutes.post("/signup", resolveCompany, UserController.store);
+authRoutes.post("/login", resolveCompany, SessionController.store);
 authRoutes.post("/refresh_token", SessionController.update);
 authRoutes.delete("/logout", isAuth, SessionController.remove);
 authRoutes.get("/me", isAuth, SessionController.me);
