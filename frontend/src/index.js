87b277d1e6894b9473b1455a5c7fc4aaa668e32a
De Alencar <lucasag0408@outlook.com>
2026-03-31 01:39:20 -0400

    only code formating

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import * as serviceworker from './serviceWorker'
+import * as serviceworker from "./serviceWorker";
 
 import App from "./App";
-
+console.log(`index`);
 ReactDOM.render(
-	<CssBaseline>
-		<App />
-	</CssBaseline>,
-	document.getElementById("root"),
-	() => {
-		window.finishProgress();
-	}
+  <CssBaseline>
+    <App />
+  </CssBaseline>,
+  document.getElementById("root"),
+  () => {
+    window.finishProgress();
+  },
 );
 
-serviceworker.register()
+serviceworker.register();
